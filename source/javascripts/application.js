a16d8dd3616ae81602fbe7117d056afe4ea82d65
rcayabyab <cayabyab.ryan@gmail.com>
2015-04-25 17:02:17 -0400

    responsive hero

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -4,12 +4,17 @@ $(document).ready(function(){
 
   var screenheight = $(window).height();
 
-  $('section.work').css("margin-top", screenheight);
-
   $('.hamburger').click(function(e) {
     e.preventDefault();
     $('header nav ul').toggleClass('show_nav');
     return $(this).toggleClass('menuopen');
   });
+
+  if ( $(window).width() > 850) { // MEDIUM
+    $('section.work').css("margin-top", screenheight);
+  }
+  else {
+    $('section.work').css("margin-top", 0);
+  }
 
 });
